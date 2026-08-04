9cdfc34de8b5c513d0e8182a85385af3fa26eab2
Kacper Zagórny <kacper@MacBook-Air-Kacper.local>
2026-08-04 21:17:01 +0200

    Suchary CLI

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sucharycli
 
 go 1.26.5
+
+require github.com/SucharyAPI/sucharypl-go v1.0.1 
--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
 
+	sucharygo "github.com/SucharyAPI/sucharypl-go"
+)
+
+func main() {
+	komenda := ""
+	if len(os.Args) > 1 {
+		komenda = os.Args[1]
+	}
+
+	if komenda == "" {
+		zart, err := sucharygo.DajKawal()
+		if err != nil {
+			obsluzBlad(err)
+			return
+		}
+		fmt.Println(zart)
+		return
+	}
+
+	var czekator sucharygo.Kategoria
+	switch komenda {
+	case "programowanie":
+		czekator = sucharygo.Programowanie
+	case "dwuznaczny":
+		czekator = sucharygo.Dwuznaczne
+	case "szkolny":
+		czekator = sucharygo.Szkolne
+	case "suchar":
+		czekator = sucharygo.Suchary
+	case "wulgarny":
+		czekator = sucharygo.Wulgarne
+	case "czarny":
+		czekator = sucharygo.CzarnyHumor
+	case "inny":
+		czekator = sucharygo.Inne
+	default:
+		fmt.Printf("Nieznana kategoria: %s\n", komenda)
+		fmt.Println("Dostępne kategorie: programowanie, dwuznaczny, szkolny, suchar, wulgarny, czarny, inny")
+		return
+	}
+
+	zart, err := sucharygo.DajKawal(czekator)
+	if err != nil {
+		obsluzBlad(err)
+		return
+	}
+	fmt.Println(zart)
+}
+
+func obsluzBlad(err error) {
+	var blednyHttp *sucharygo.HTTPError
+	var blednePolaczenie *sucharygo.ConnectionError
+	var blednyJson *sucharygo.JSONError
+
+	if errors.As(err, &blednyHttp) {
+		fmt.Printf("Błąd serwera HTTP (%d): %s\n", blednyHttp.Kod, blednyHttp.Tresc)
+		return
+	}
+
+	if errors.As(err, &blednePolaczenie) {
+		fmt.Println("Błąd połączenia z siecią! Sprawdź swoje połączenie z internetem.")
+		return
+	}
+
+	if errors.As(err, &blednyJson) {
+		fmt.Println("Błąd przetwarzania danych JSON z odpowiedzi API.")
+		return
+	}
+
+	fmt.Printf("Wystąpił nieoczekiwany błąd: %v\n", err)
 }
